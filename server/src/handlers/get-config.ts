5492dcc2b745e73f381221a3b891f39b57204809
Sebastian Enns <sebastian.enns@mni.thm.de>
2026-05-29 00:06:58 +0200

    Fixed packages and server.

--- server/src/handlers/get-config.ts
+++ server/src/handlers/get-config.ts
@@ -1,8 +1,8 @@
 import { Request, Response } from "express";
-import { ConfigService, JSON } from "../services/ConfigService";
+import { Config, ConfigService } from "../services/ConfigService";
 import { STATUS_CODE } from "../constants/STATUS_CODE";
 
 export async function getConfig(_: Request, res: Response): Promise<void> {
-  const config: JSON = await ConfigService.fetchConfig();
+  const config: Config = await ConfigService.fetchConfig();
   res.status(STATUS_CODE.OK).send(config);
 }
