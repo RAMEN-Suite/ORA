aae9ed0d9e8e2434f39adb1d86905607301bb5fa
Sebastian Enns <sebastian.enns@mni.thm.de>
2026-06-01 21:09:40 +0200

    Fixed language popover (primeNg bug).

--- client/src/app/view/shared/navbar/navbar.component.ts
+++ client/src/app/view/shared/navbar/navbar.component.ts
@@ -8,6 +8,7 @@ import { TranslocoDirective } from '@jsverse/transloco';
 import { AVAILABLE_LANGUAGES, LanguageKey, NavItem, SiteOptions } from '../../../models/config/SiteOptions';
 import { ConfigService } from '../../../services/config.service';
 import { LanguageService } from '../../../services/language.service';
+import { Nullable } from 'primeng/ts-helpers';
 
 @Component({
   selector: 'shared-navbar',
@@ -22,12 +23,20 @@ export class NavbarComponent {
   protected readonly image: Signal<string | undefined> = computed((): string | undefined => this.config.navbar.image);
   protected readonly navItems: Signal<NavItem[]> = computed((): NavItem[] => this.config.navbar.items ?? []);
 
+  protected readonly activeLanguage: Signal<LanguageKey | undefined> = this.languageService.activeLanguage;
   protected readonly languages: Signal<LanguageKey[]> = computed((): LanguageKey[] => {
     const languages: LanguageKey[] = this.config.language.available;
     return AVAILABLE_LANGUAGES.filter((key: LanguageKey): boolean => languages.includes(key));
   });
 
-  protected readonly activeLanguage: Signal<LanguageKey | undefined> = this.languageService.activeLanguage;
+  protected handleOpenLanguages(popover: Popover, event: Event): void {
+    popover.toggle(event);
+
+    requestAnimationFrame((): void => {
+      popover.align();
+      this.resetPopoverOffset(popover);
+    });
+  }
 
   protected handleLanguageChange(language: LanguageKey, popover: Popover): void {
     popover.hide();
@@ -36,5 +45,12 @@ export class NavbarComponent {
 
   protected isExternal(item: NavItem): boolean {
     return !!item.href && /^(https?:)?\/\//.test(item.href);
+  }
+
+  private resetPopoverOffset(popover: Popover): void {
+    const container: Nullable<HTMLElement> = popover.container;
+    if (!container) return;
+    container.style.top = `${container.offsetTop - window.scrollY}px`;
+    container.style.left = `${container.offsetLeft + 15 - window.scrollX}px`;
   }
 }
